74813645fdb0843e00f36cc0e15d5e697a4e0502
Clarence Lee <clarence.lee@ovoenergy.com>
2019-01-31 16:57:26 +0000

    setup apollo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { mount, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  const component = mount(<App />);
+  expect(component.text()).toBe("Hello");
 });
